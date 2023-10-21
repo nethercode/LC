19b191824c8aa3a9e4f7cabeb8b8eb8c35041402
Nethercode <github-user@hotmail.com>
2023-10-20 22:54:27 -0500

    00004

--- 00004/code.js
+++ 00004/code.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 var findMedianSortedArrays = function(nums1, nums2) {
     if (nums1.length > nums2.length) {
         return findMedianSortedArrays(nums2, nums1);
